034e1309ee0c923967f580475315318964e46536
Misha <kam628326@gmail.com>
2024-07-09 11:21:11 +1000

    autorisation

--- php/autorisation.php
+++ php/autorisation.php
@@ -1,4 +1,6 @@
 <?php
+    $message_error = $_POST['message'];
+
     $title = 'Вход в аккаунт';
 
     require('_header.php');
@@ -13,10 +15,18 @@
             <input type="text" name="login" placeholder="Имя пользователя" class="text">
             <p>Введите пароль</p>
             <input type="password" name="password" placeholder='Пароль' class="text">
+            <?php
+                if (!empty($message_error)) {
+                    ?>
+                    <p style="font-size: 12pt; color: red; text-align: right; margin-top: 10px;">Логин или пароль<br>введены не верно</p>
+                    <?php
+                }
+            ?>
             <p>
                 <a href="">Забыли пароль?</a>
                 <input type="submit" value="Войти" id="submit">
             </p>
+            
     </div>
 </section>
 
--- php/signin.php
+++ php/signin.php
@@ -4,10 +4,39 @@
 
     require("connect.php");
 
-    $stmt = $BD->prepare("SELECT * FROM `users` WHERE login = ?");
+    $stmt = $BD->prepare("SELECT password FROM `users` WHERE login = ?");
 
-    $stmt->bind_param
+    $stmt->bind_param("s", $login);
 
-    $check_login = mysqli_query($BD, "SELECT * FROM `users` WHERE login=$login");
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $result_password = $result->fetch_assoc();
+
+        if($result_password["password"] == $password) {
+            echo '
+            <form id="Successful" method="post" action="../index.php">
+                <input type="hidden" name="message" value="successful">
+             </form>
+            <script type="text/javascript">
+                document.getElementById("Successful").submit();
+            </script>
+            ';
+        }
+    }
+    
+    echo '
+        <form id="ErrorMessage" method="post" action="autorisation.php">
+            <input type="hidden" name="message" value="Error">
+        </form>
+        <script type="text/javascript">
+            document.getElementById("ErrorMessage").submit();
+        </script>';
+
+    $stmt->close();
+    
     print($check_login);
 ?>
+
